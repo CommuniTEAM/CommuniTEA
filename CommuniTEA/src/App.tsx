59a83571fdaf181429624737a8b6a38a1ce42ed1
Cory DeGuzman <deguzmancory@ymail.com>
2023-11-07 17:22:39 -0800

    Test commit

--- CommuniTEA/src/App.tsx
+++ CommuniTEA/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-    <h1>Welcome CommuniTEAM Members!</h1>
+    <h1>Welcome CommuniTEAM Members! HELLO</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
