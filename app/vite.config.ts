4178521a92523261d4e99f3bc7599d7e803c7aa8
Angela Fisher <31549337+angelajfisher@users.noreply.github.com>
2023-11-07 21:22:06 -0800

    Enable Vite hot reload

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -7,5 +7,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+    // Look into whether watch option (hot reload) should be removed for production
+    watch: {
+      usePolling: true
+    }
   }
 })
