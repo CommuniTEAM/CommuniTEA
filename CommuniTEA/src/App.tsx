b9e66619d25676f4791e70dec3a961cad9482f72
deguzmancoryDH <cory_deguzman@datahouse.com>
2023-11-07 22:58:19 -0800

    TEA-1 THIS IS A TEST AGAIN

--- CommuniTEA/src/App.tsx
+++ CommuniTEA/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
 	return (
 		<>
-			<h1>Welcome CommuniTEAM Members! THIS IS A TEST</h1>
+			<h1>Welcome CommuniTEAM Members! THIS IS A TEST. HELLO.</h1>
 			<div>
 				<a href="https://vitejs.dev" target="_blank">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
