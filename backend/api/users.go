c43cc6b91681ad6b8302f9ae4781f39119eb4361
Angela Fisher <31549337+angelajfisher@users.noreply.github.com>
2023-11-27 07:53:36 -0800

    🚧 [TEA-52]: Initial API setup Set up sqlc, pgx, swaggest/rest, and dbmate so that they at least work and connect to each other where applicable. Things are messy, but the foundation is in place! Pair-programmed with @BMLx23

---
+++ backend/api/users.go
@@ -0,0 +1,71 @@
+package api
+
+import (
+	"context"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/swaggest/usecase"
+	// "github.com/swaggest/usecase/status"
+	"github.com/CommuniTEAM/CommuniTEA/db/sqlc"
+)
+
+// Declare input port type.
+type userInput struct {
+	Name string `path:"name" minLength:"3"` // Field tags define parameter location and JSON schema constraints.
+
+	// Field tags of unnamed fields are applied to parent schema.
+	// they are optional and can be used to disallow unknown parameters.
+	// For non-body params, name tag must be provided explicitly.
+	// E.g. here no unknown `query` and `cookie` parameters allowed,
+	// unknown `header` params are ok.
+	_ struct{} `query:"_" cookie:"_" additionalProperties:"false"`
+}
+
+// Declare output port type.
+type userOutput struct {
+	Now     time.Time `header:"X-Now" json:"-"`
+	Message string    `json:"message"`
+}
+
+// func CreateUser() usecase.IOInteractorOf[userInput, userOutput] {
+// 	// Create use case interactor with references to input/output types and interaction function.
+// 	u := usecase.NewInteractor(func(ctx context.Context, input userInput, output *userOutput) error {
+
+// 		output.Message = fmt.Sprintf(msg, input.Name)
+// 		output.Now = time.Now()
+
+// 		return nil
+// 	})
+
+// 	// Describe use case interactor.
+// 	u.SetTitle("Create User")
+// 	u.SetDescription("Make a new user (requires a name).")
+
+// 	u.SetExpectedErrors(status.InvalidArgument)
+
+// 	return u
+// }
+
+func GetAllUsers() usecase.Interactor {
+	response := usecase.NewInteractor(func(ctx context.Context, _ struct{}, output *[]db.User) error {
+
+		dbCtx := context.Background()
+
+		conn, err := pgx.Connect(dbCtx, "postgresql://admin:secret@postgres/communitea-db")
+		if err != nil {
+			return err
+		}
+		defer conn.Close(dbCtx)
+		queries := db.New(conn)
+
+        *output, err = queries.GetUsers(ctx)
+		if err != nil {
+			return err
+		}
+        return nil
+    })
+    response.SetTags("Users")
+
+    return response
+}
--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,32 +1,72 @@
+// Documentation you'll need to get acquainted with the APIs:
+// https://pkg.go.dev/github.com/swaggest/rest#section-readme
+// https://docs.sqlc.dev/en/latest/tutorials/getting-started-postgresql.html
+// https://github.com/jackc/pgx/wiki/Getting-started-with-pgx
+// https://dev.to/vearutop/tutorial-developing-a-restful-api-with-go-json-schema-validation-and-openapi-docs-2490
+
 package main
 
 import (
-	    "net/http"
-        "fmt"
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	"net/http"
+	"log"
+
+	"github.com/swaggest/openapi-go/openapi31"
+	"github.com/swaggest/rest/response/gzip"
+	"github.com/swaggest/rest/web"
+	swgui "github.com/swaggest/swgui/v5emb"
+
+	"github.com/CommuniTEAM/CommuniTEA/api"
 )
 
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Gorilla!\n"))
+}
+
 func main() {
-fmt.Println("HELLO WORLD!")
- // Create a new request multiplexer
- // Take incoming requests and dispatch them to the matching handlers
- mux := http.NewServeMux()
+	conn, err := pgx.Connect(context.Background(), "postgresql://admin:secret@postgres/communitea-db")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
 
- // Register the routes and handlers
- mux.Handle("/", &homeHandler{})
- mux.Handle("/test", &testHandler{})
+	var greeting string
+	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
 
- // Run the server
- http.ListenAndServe(":8000", mux)
-}
+	fmt.Println(greeting)
 
-type homeHandler struct{}
 
-func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
- w.Write([]byte("This is my home page"))
-}
+	s := web.NewService(openapi31.NewReflector())
 
-type testHandler struct{}
+	// Init API documentation schema.
+	s.OpenAPISchema().SetTitle("CommuniTEA API")
+	s.OpenAPISchema().SetDescription("Let's gooooooooooooooo!")
+	s.OpenAPISchema().SetVersion("v1.0.0")
 
-func (t *testHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
- w.Write([]byte("This is another test message"))
+	// Setup middlewares.
+	s.Wrap(
+		gzip.Middleware, // Response compression with support for direct gzip pass through.
+	)
+
+	// Add use case handler to router.
+	s.Get("/hello/{name}", api.Greet())
+	s.Get("/users", api.GetAllUsers())
+
+	// Swagger UI endpoint at /docs.
+	s.Docs("/docs", swgui.New)
+
+	// Start server.
+	log.Println("http://localhost:8000/docs")
+	if err := http.ListenAndServe(":8000", s); err != nil {
+		log.Fatal(err)
+	}
 }
