5638ca5eec262ba15f9167ef42e78e15470a585a
Brian <bmla.pt@gmail.com>
2023-11-30 14:14:16 -0800

    ✨ [TEA-52]: Added create user endpoint

--- backend/api/users.go
+++ backend/api/users.go
@@ -2,68 +2,120 @@ package api
 
 import (
 	"context"
-	"time"
+	"fmt"
 
-	// "github.com/swaggest/usecase/status"
 	db "github.com/CommuniTEAM/CommuniTEA/db/sqlc"
 	"github.com/jackc/pgx/v5"
 	"github.com/swaggest/usecase"
+	"github.com/swaggest/usecase/status"
 )
 
 // Declare input port type.
+
 type userInput struct {
-	Name string `minLength:"3" path:"name"` // Field tags define parameter location and JSON schema constraints.
+	Name string `json:"name"` // Field tags define parameter location and JSON schema constraints.
 
 	// Field tags of unnamed fields are applied to parent schema.
+
 	// they are optional and can be used to disallow unknown parameters.
+
 	// For non-body params, name tag must be provided explicitly.
+
 	// E.g. here no unknown `query` and `cookie` parameters allowed,
+
 	// unknown `header` params are ok.
-	_ struct{} `query:"_" cookie:"_" additionalProperties:"false"`
+
+	// _ struct{} `query:"_" cookie:"_" additionalProperties:"false"`
+
 }
 
 // Declare output port type.
-type userOutput struct {
-	Now     time.Time `header:"X-Now" json:"-"`
-	Message string    `json:"message"`
-}
 
-// func CreateUser() usecase.IOInteractorOf[userInput, userOutput] {
-// 	// Create use case interactor with references to input/output types and interaction function.
-// 	u := usecase.NewInteractor(func(ctx context.Context, input userInput, output *userOutput) error {
+// type userOutput struct {
+// 	Now time.Time `header:"X-Now" json:"-"`
 
-// 		output.Message = fmt.Sprintf(msg, input.Name)
-// 		output.Now = time.Now()
-
-// 		return nil
-// 	})
-
-// 	// Describe use case interactor.
-// 	u.SetTitle("Create User")
-// 	u.SetDescription("Make a new user (requires a name).")
-
-// 	u.SetExpectedErrors(status.InvalidArgument)
-
-// 	return u
+// 	Name string `json:"name"`
 // }
+
+// Displays New User Response
+
+// type newUserResponse(user db.User) struct {
+
+// 	return userOutput{
+
+// 		Now: user.Now,
+
+// 		Name: user.Name,
+
+// 	}
+
+// }
+
+func CreateUser() usecase.Interactor {
+	// Create use case interactor with references to input/output types and interaction function.
+
+	response := usecase.NewInteractor(func(ctx context.Context, input userInput, output *db.User) error {
+		dbCtx := context.Background()
+
+		conn, err := pgx.Connect(dbCtx, "postgresql://admin:secret@postgres/communitea-db")
+
+		if err != nil {
+			return fmt.Errorf("failed to connect to DB: %w", err)
+		}
+
+		defer conn.Close(dbCtx)
+
+		queries := db.New(conn)
+
+		name := "%s"
+
+		input.Name = fmt.Sprintf(name, input.Name)
+
+		*output, err = queries.CreateUser(ctx, input.Name)
+
+		if err != nil {
+			return fmt.Errorf("failed to create user: %w", err)
+		}
+
+		return nil
+
+		// output.Now = time.Now()
+	})
+
+	// Describe use case interactor.
+
+	response.SetTitle("Create User")
+
+	response.SetDescription("Make a new user (requires a name).")
+
+	response.SetExpectedErrors(status.InvalidArgument)
+
+	return response
+}
 
 func GetAllUsers() usecase.Interactor {
 	response := usecase.NewInteractor(func(ctx context.Context, _ struct{}, output *[]db.User) error {
 		dbCtx := context.Background()
 
 		conn, err := pgx.Connect(dbCtx, "postgresql://admin:secret@postgres/communitea-db")
+
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to connect to DB: %w", err)
 		}
+
 		defer conn.Close(dbCtx)
+
 		queries := db.New(conn)
 
 		*output, err = queries.GetUsers(ctx)
+
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to get all user from DB: %w", err)
 		}
+
 		return nil
 	})
+
 	response.SetTags("Users")
 
 	return response
--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,20 +1,21 @@
 // Documentation you'll need to get acquainted with the APIs:
+
 // https://pkg.go.dev/github.com/swaggest/rest#section-readme
+
 // https://docs.sqlc.dev/en/latest/tutorials/getting-started-postgresql.html
+
 // https://github.com/jackc/pgx/wiki/Getting-started-with-pgx
+
 // https://dev.to/vearutop/tutorial-developing-a-restful-api-with-go-json-schema-validation-and-openapi-docs-2490
 
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
+	"time"
 
 	"github.com/CommuniTEAM/CommuniTEA/api"
-	"github.com/jackc/pgx/v5"
 	"github.com/swaggest/openapi-go/openapi31"
 	"github.com/swaggest/rest/response/gzip"
 	"github.com/swaggest/rest/web"
@@ -22,44 +23,50 @@ import (
 )
 
 func main() {
-	conn, err := pgx.Connect(context.Background(), "postgresql://admin:secret@postgres/communitea-db")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
-	}
-	defer conn.Close(context.Background())
-
-	var greeting string
-	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Println(greeting)
-
 	s := web.NewService(openapi31.NewReflector())
 
 	// Init API documentation schema.
+
 	s.OpenAPISchema().SetTitle("CommuniTEA API")
+
 	s.OpenAPISchema().SetDescription("Let's gooooooooooooooo!")
+
 	s.OpenAPISchema().SetVersion("v1.0.0")
 
 	// Setup middlewares.
+
 	s.Wrap(
+
 		gzip.Middleware, // Response compression with support for direct gzip pass through.
+
 	)
 
 	// Add use case handler to router.
+
 	s.Get("/hello/{name}", api.Greet())
+
 	s.Get("/users", api.GetAllUsers())
 
+	s.Post("/users", api.CreateUser())
+
 	// Swagger UI endpoint at /docs.
+
 	s.Docs("/docs", swgui.New)
 
 	// Start server.
+
 	log.Println("http://localhost:8000/docs")
-	if err := http.ListenAndServe(":8000", s); err != nil {
-		log.Fatal(err)
+
+	const three = 3
+
+	// Run the server
+	server := &http.Server{
+		Addr:              ":8000",
+		ReadHeaderTimeout: three * time.Second,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
 	}
 }
