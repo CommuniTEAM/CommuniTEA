ec81002c7b4d4236fedc03e3322e41ab06e92d36
Angela Fisher <31549337+angelajfisher@users.noreply.github.com>
2023-12-11 22:47:30 -0800

    ✨ [TEA-20]: Add users tables; remove example code

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,11 +1,7 @@
 // Documentation you'll need to get acquainted with the APIs:
-
 // https://pkg.go.dev/github.com/swaggest/rest#section-readme
-
 // https://docs.sqlc.dev/en/latest/tutorials/getting-started-postgresql.html
-
 // https://github.com/jackc/pgx/wiki/Getting-started-with-pgx
-
 // https://dev.to/vearutop/tutorial-developing-a-restful-api-with-go-json-schema-validation-and-openapi-docs-2490
 
 package main
@@ -25,8 +21,8 @@ import (
 )
 
 func main() {
+	// Init database connection pool
 	dbPool, err := db.NewDBPool(os.Getenv("DB_URI"))
-
 	if err != nil {
 		panic(err)
 	}
@@ -34,41 +30,30 @@ func main() {
 	s := web.NewService(openapi31.NewReflector())
 
 	// Init API documentation schema.
-
 	s.OpenAPISchema().SetTitle("CommuniTEA API")
-
 	s.OpenAPISchema().SetDescription("Bringing your community together over a cuppa")
-
 	s.OpenAPISchema().SetVersion("v0.0.1")
 
 	// Setup middlewares.
-
 	s.Wrap(
-
 		gzip.Middleware, // Response compression with support for direct gzip pass through.
-
 	)
 
-	// Add use case handler to router.
-
+	// Add API endpoints to router.
+	// greeter (example endpoint to be removed for prod)
 	s.Get("/hello/{name}", api.Greet())
 
-	s.Get("/users", api.GetAllUsers(dbPool))
-
-	s.Post("/users", api.CreateUser(dbPool))
-
+	// locations
 	s.Post("/locations/cities", api.CreateCity(dbPool))
 
+	// wikiteadia
 	s.Get("/teas/{published}", api.GetAllTeas(dbPool))
-
 	s.Post("/teas", api.CreateTea(dbPool))
 
 	// Swagger UI endpoint at /docs.
-
 	s.Docs("/docs", swgui.New)
 
 	// Start server.
-
 	pubURL := os.Getenv("PUBLIC_URL")
 
 	if pubURL == "" {
@@ -78,20 +63,13 @@ func main() {
 	}
 
 	// Run the server
-
 	const serverTimeout = 5
-
 	server := &http.Server{
-
-		Addr: ":8000",
-
-		Handler: s,
-
+		Addr:              ":8000",
+		Handler:           s,
 		ReadHeaderTimeout: serverTimeout * time.Second,
 	}
-
 	err = server.ListenAndServe()
-
 	if err != nil {
 		panic(err)
 	}
