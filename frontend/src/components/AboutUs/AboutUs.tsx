30c82bb155479eb1186e777eb7e7f0ac40225980
Cory DeGuzman <deguzmancory@ymail.com>
2023-12-12 20:49:21 -0800

    🔨 [TEA-64]: Refactor

--- frontend/src/components/AboutUs/AboutUs.tsx
+++ frontend/src/components/AboutUs/AboutUs.tsx
@@ -1,15 +1,15 @@
+import Footer from '../LandingPage/Footer';
 import NavBar from '../LandingPage/Navbar';
 import HeroBanner from './HeroBanner';
 import TeamMemberCarousel from './TeamMemberCarousel';
 
 export default function AboutUs(): JSX.Element {
   return (
-    <>
+    <div style={{ overflowX: 'hidden' }}>
       <NavBar />
       <HeroBanner />
       <TeamMemberCarousel />
-      <h1>ABOUT US PAGE</h1>
-      <p>Add stuff here</p>
-    </>
+      <Footer />
+    </div>
   );
 }
