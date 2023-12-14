2db582b61da94cd8d4d2bf7e71bf6b3635260b0f
BMLx23 <134474081+BMLx23@users.noreply.github.com>
2023-12-14 08:56:04 -0800

    [TEA-21]: Migrations: Business Tables (#26)
    ✨ [TEA-21]: added business tables

--- backend/db/dbpool.go
+++ backend/db/dbpool.go
@@ -12,17 +12,23 @@ import (
 // connection pool and returns its reference
 
 func NewDBPool(connString string) (*pgxpool.Pool, error) {
+
 	pgxConfig, err := pgxpool.ParseConfig(connString)
 
 	if err != nil {
+
 		return nil, fmt.Errorf("could not parse db conn string: %w", err)
+
 	}
 
 	pgxDBPool, err := pgxpool.NewWithConfig(context.TODO(), pgxConfig)
 
 	if err != nil {
+
 		return nil, fmt.Errorf("could not create new db pool: %w", err)
+
 	}
 
 	return pgxDBPool, nil
+
 }
