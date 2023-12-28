59ffddf67df45fdc2093427b18e2e2350135d466
Cory DeGuzman <deguzmancory@ymail.com>
2023-12-27 19:20:49 -0800

    🔨 [TEA-67]: Landing Page: Business Section

--- frontend/src/components/LandingPage/BusinessShowcase.tsx
+++ frontend/src/components/LandingPage/BusinessShowcase.tsx
@@ -1,73 +1,46 @@
 import { Button } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 import BusinessPartnerCards from './Cards/BusinessPartnerCards';
+import './styles/BusinessShowcaseStyles.css';
 
 export default function BusinessShowcase(): JSX.Element {
+  const theme = useTheme();
+
   return (
-    <>
-      <div>
+    <div className="businessShowcaseContainer">
+      <div className="businessShowcaseTextContainer">
+        <h1 className="businessShowcaseTitle">BUSINESSES</h1>
+        <p className="businessShowcaseDescription">
+          Showcase your unique tea selections to a wider audience and boost
+          community engagement. Join CommuniTEA and let tea lovers discover and
+          enjoy your offerings!
+        </p>
         <Button
+          className="businessShowcaseButton"
           variant="contained"
           sx={{
             backgroundColor: '#8B4513',
             fontFamily: 'Montserrat',
-            borderRadius: 10,
-            fontSize: '1vw',
-            padding: 2.5,
-            transform: 'translate(7vw, 2vw)',
+            borderRadius: '50px',
+            fontSize: '1.5rem',
+            padding: '20px 40px',
             '&:hover': {
               backgroundColor: '#D2B48C',
               color: 'black',
+            },
+            [theme.breakpoints.down('md')]: {
+              fontSize: '1rem',
             },
           }}
         >
           Embark. Unite. Elevate your CommuniTEA.
         </Button>
       </div>
-      <div
-        style={{
-          // height: responsiveHeight,
-          backgroundColor: 'rgba(85, 107, 47, 0.6)',
-          display: 'flex',
-        }}
-      >
-        <div style={{ flexGrow: 1, maxWidth: '50%' }}>
-          <div style={{ width: '80%', transform: 'translate(4vw, 5vw)' }}>
-            <h1
-              style={{
-                fontFamily: 'Roboto Slab',
-                fontSize: '3vw',
-                fontWeight: 700,
-              }}
-            >
-              BUSINESSES
-            </h1>
-            <p
-              style={{
-                fontFamily: 'Roboto',
-                fontSize: '1.75vw',
-                fontWeight: 300,
-              }}
-            >
-              Showcase your unique tea selections to a wider audience and boost
-              community engagement. Join CommuniTEA and let tea lovers discover
-              and enjoy your offerings!
-            </p>
-          </div>
-        </div>
-        <div
-          style={{
-            flexGrow: 1,
-            maxWidth: '60%',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-          }}
-        >
-          <div>
-            <BusinessPartnerCards />
-          </div>
+      <div className="businessPartnerCardsContainer">
+        <div>
+          <BusinessPartnerCards />
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- frontend/src/components/LandingPage/HowItWorks.tsx
+++ frontend/src/components/LandingPage/HowItWorks.tsx
@@ -10,31 +10,37 @@ export default function HowItWorks(): JSX.Element {
       <h1 className="howItWorksTitle">How does it work?</h1>
       <Grid container spacing={2} justifyContent="center">
         {/* Grid item for Step One */}
-        <Grid item xs={12} sm={6} md={4} className="howItWorksGridItem">
-          <img src={One} alt="Step one" className="howItWorksImage" />
-          <h1 className="howItWorksStepTitle">Discover</h1>
-          <p className="howItWorksDescription">
-            Find your favorite teas from curated local businesses
-          </p>
+        <Grid item xs={12} sm={6} md={4}>
+          <div className="howItWorksGridItem">
+            <img src={One} alt="Step one" className="howItWorksImage" />
+            <h1 className="howItWorksStepTitle">Discover</h1>
+            <p className="howItWorksDescription">
+              Find your favorite teas from curated local businesses
+            </p>
+          </div>
         </Grid>
 
         {/* Grid item for Step Two */}
-        <Grid item xs={12} sm={6} md={4} className="howItWorksGridItem">
-          <img src={Two} alt="Step two" className="howItWorksImage" />
-          <h1 className="howItWorksStepTitle">Connect</h1>
-          <p className="howItWorksDescription">
-            Explore tea-related events and connect enthusiasts.
-          </p>
+        <Grid item xs={12} sm={6} md={4}>
+          <div className="howItWorksGridItem">
+            <img src={Two} alt="Step two" className="howItWorksImage" />
+            <h1 className="howItWorksStepTitle">Connect</h1>
+            <p className="howItWorksDescription">
+              Explore tea-related events and connect enthusiasts.
+            </p>
+          </div>
         </Grid>
 
         {/* Grid item for Step Three */}
-        <Grid item xs={12} sm={6} md={4} className="howItWorksGridItem">
-          <img src={Three} alt="Step three" className="howItWorksImage" />
-          <h1 className="howItWorksStepTitle">Experience</h1>
-          <p className="howItWorksDescription">
-            Transform your tea-ppreciation into a collective experience with
-            CommuniTEA.
-          </p>
+        <Grid item xs={12} sm={6} md={4}>
+          <div className="howItWorksGridItem">
+            <img src={Three} alt="Step three" className="howItWorksImage" />
+            <h1 className="howItWorksStepTitle">Experience</h1>
+            <p className="howItWorksDescription">
+              Transform your tea-ppreciation into a collective experience with
+              CommuniTEA.
+            </p>
+          </div>
         </Grid>
       </Grid>
     </div>
