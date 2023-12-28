aeadeda93bb9f8e3adfc1fcdf23112dc43defba5
Cory DeGuzman <deguzmancory@ymail.com>
2023-12-27 17:32:51 -0800

    🔨 [TEA-67]: Landing Page: How it works

--- frontend/src/components/LandingPage/HowItWorks.tsx
+++ frontend/src/components/LandingPage/HowItWorks.tsx
@@ -2,103 +2,38 @@ import { Grid } from '@mui/material';
 import One from '../../assets/One.svg';
 import Two from '../../assets/Two.svg';
 import Three from '../../assets/Three.svg';
-import '../../App.css';
+import './styles/HowItWorksStyles.css';
 
 export default function HowItWorks(): JSX.Element {
   return (
-    <div>
-      <h1
-        style={{
-          fontFamily: 'Roboto Slab',
-          fontSize: '2vw',
-          fontWeight: 300,
-          color: '#416543',
-          textAlign: 'center',
-        }}
-      >
-        How does it work?
-      </h1>
+    <div className="container">
+      <h1 className="howItWorksTitle">How does it work?</h1>
       <Grid container spacing={2} justifyContent="center">
-        <Grid item xs={12} sm={6} md={4} style={{ textAlign: 'center' }}>
-          <img
-            src={One}
-            alt="Step one"
-            style={{ width: '15vw', height: 'auto', paddingTop: '5vh' }}
-          />
-          <h1
-            style={{
-              fontFamily: 'Roboto Slab',
-              fontSize: '1.5vw',
-              fontWeight: 300,
-              color: '#416543',
-            }}
-          >
-            Discover
-          </h1>
-          <p
-            style={{
-              fontFamily: 'Roboto',
-              fontSize: '1vw',
-              fontWeight: 300,
-              color: '#416543',
-            }}
-          >
+        {/* Grid item for Step One */}
+        <Grid item xs={12} sm={6} md={4} className="howItWorksGridItem">
+          <img src={One} alt="Step one" className="howItWorksImage" />
+          <h1 className="howItWorksStepTitle">Discover</h1>
+          <p className="howItWorksDescription">
             Find your favorite teas from curated local businesses
           </p>
         </Grid>
-        <Grid item xs={12} sm={6} md={4} style={{ textAlign: 'center' }}>
-          <img
-            src={Two}
-            alt="Step two"
-            style={{ width: '15vw', height: 'auto', paddingTop: '5vh' }}
-          />
-          <h1
-            style={{
-              fontFamily: 'Roboto Slab',
-              fontSize: '1.5vw',
-              fontWeight: 300,
-              color: '#416543',
-            }}
-          >
-            Connect
-          </h1>
-          <p
-            style={{
-              fontFamily: 'Roboto',
-              fontSize: '1vw',
-              fontWeight: 300,
-              color: '#416543',
-            }}
-          >
+
+        {/* Grid item for Step Two */}
+        <Grid item xs={12} sm={6} md={4} className="howItWorksGridItem">
+          <img src={Two} alt="Step two" className="howItWorksImage" />
+          <h1 className="howItWorksStepTitle">Connect</h1>
+          <p className="howItWorksDescription">
             Explore tea-related events and connect enthusiasts.
           </p>
         </Grid>
-        <Grid item xs={12} sm={6} md={4} style={{ textAlign: 'center' }}>
-          <img
-            src={Three}
-            alt="Step three"
-            style={{ width: '15vw', height: 'auto', paddingTop: '5vh' }}
-          />
-          <h1
-            style={{
-              fontFamily: 'Roboto Slab',
-              fontSize: '1.5vw',
-              fontWeight: 300,
-              color: '#416543',
-            }}
-          >
-            Experience
-          </h1>
-          <p
-            style={{
-              fontFamily: 'Roboto',
-              fontSize: '1vw',
-              fontWeight: 300,
-              color: '#416543',
-            }}
-          >
+
+        {/* Grid item for Step Three */}
+        <Grid item xs={12} sm={6} md={4} className="howItWorksGridItem">
+          <img src={Three} alt="Step three" className="howItWorksImage" />
+          <h1 className="howItWorksStepTitle">Experience</h1>
+          <p className="howItWorksDescription">
             Transform your tea-ppreciation into a collective experience with
-            CommuniTEA
+            CommuniTEA.
           </p>
         </Grid>
       </Grid>
