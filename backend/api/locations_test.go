351d23284e1d7d81e1e96ae65a5a8dfd25b64967
Angela Fisher <ajf-noreply@phantom-mail.io>
2024-01-08 21:40:44 -0800

    ✅ [TEA-62]: Test for GetCity

--- backend/api/locations_test.go
+++ backend/api/locations_test.go
@@ -40,3 +40,39 @@ func (suite *LocationsTestSuite) TestGetAllStates() {
 
 	assertjson.Equal(t, expectedBody, body)
 }
+
+func (suite *LocationsTestSuite) TestGetCity() {
+	t := suite.T()
+
+	// ID of the manually added Seattle entry
+	cityID := "4c33e0bc-3d43-4e77-aed0-b7aff09bb689"
+
+	// Check 200 response & body
+	req, err := http.NewRequest(http.MethodGet, suite.server.URL+"/locations/cities/"+cityID, nil)
+	require.NoError(t, err)
+
+	resp, err := http.DefaultTransport.RoundTrip(req)
+	require.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	body, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+	require.NoError(t, resp.Body.Close())
+
+	expectedBody, err := os.ReadFile("_testdata/locations/get_city.json")
+	require.NoError(t, err)
+
+	assertjson.Equal(t, expectedBody, body)
+
+	// Check 404 response
+	cityID = "4c33e0bc-3d43-4e77-aed0-b7aff09bb600" // a uuid not in cities table
+
+	req, err = http.NewRequest(http.MethodGet, suite.server.URL+"/locations/cities/"+cityID, nil)
+	require.NoError(t, err)
+
+	resp, err = http.DefaultTransport.RoundTrip(req)
+	require.NoError(t, err)
+
+	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+}
